679d717791652e4d72ee1a1144cbe7fd82c42807
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-04-07 22:28:01 +0100

    add Auth container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from "./containers/Layout/Layout";
-import Registration from "./containers/Registration/Registration";
+import Auth from "./containers/Auth/Auth";
 // import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 // import Checkout from "./containers/Checkout/Checkout";
 // import Orders from "./containers/Orders/Orders";
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <Router>
       <Layout>
-        <Route path="/" exact component={Registration} />
+        <Route path="/" exact component={Auth} />
         {/* <Route path="/checkout" component={Checkout} />
         <Route path="/orders" component={Orders} />
         <Route path="/logout" component={Logout} />
