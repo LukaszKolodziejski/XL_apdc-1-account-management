ac5439fb14327dcee9f9d2798c2c5784a324462a
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-04-07 09:57:56 +0100

    install package & configuration

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">działa</div>;
+};
 
 export default App;
