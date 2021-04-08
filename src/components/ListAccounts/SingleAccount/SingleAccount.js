6a36e7c3ccb35bcef859485c808e68e1c96f0910
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-04-08 22:04:29 +0100

    show my account

--- src/components/ListAccounts/SingleAccount/SingleAccount.js
+++ src/components/ListAccounts/SingleAccount/SingleAccount.js
@@ -2,9 +2,13 @@ import React from "react";
 import styles from "./SingleAccount.module.css";
 
 const SingleAccount = (props) => {
-  const { publicUserId, email, profile, state, role } = props;
+  const { publicUserId, email, profile, state, role, myAccount } = props;
+
+  const classNames = myAccount
+    ? [styles.SingleAccount, styles.MyAccount].join(" ")
+    : styles.SingleAccount;
   return (
-    <div className={styles.SingleAccount}>
+    <div className={classNames}>
       <span>{publicUserId}</span>
       <span>{email}</span>
       <span>{profile}</span>
