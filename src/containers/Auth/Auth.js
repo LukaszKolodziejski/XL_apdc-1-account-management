56a2cba6ad2d1b12531b26136087b528238d5a9e
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-04-09 08:21:16 +0100

    remove logout button from Auth.js

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -170,7 +170,6 @@ class Auth extends Component {
               {this.state.isSignup ? "registration" : "login"}
             </h4>
             {errorMessage}
-            <button onClick={() => this.props.onLogout()}>Logout</button>
             <form onSubmit={this.formHandler}>
               {allInputs}
               <WrapperAuth x="40">
